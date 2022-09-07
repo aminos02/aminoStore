38203e1cfa9262d908a0ff7036d744ba33812bf1
Azzouz Amine <azzouzamine02@gmail.com>
2022-09-07 22:38:01 +0100

    start

--- App.js
+++ App.js
@@ -3,9 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <Text>Open Hello World</Text>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
