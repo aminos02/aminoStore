2e55220ca81ba0d1807e0145ba0ddcb277f0a190
Azzouz Amine <azzouzamine02@gmail.com>
2022-09-07 22:55:10 +0100

    add TailWindCss

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}>
+    <View className="flex-1 items-center justify-center bg-red-500">
       <Text>Open Hello World</Text>
-      {/* <StatusBar style="auto" /> */}
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
